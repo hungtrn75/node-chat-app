de9f9ea8ebaa412804a9ac93eb9874c5139c5edb
hungtrn75 <hungtrn75@gmail.com>
2018-04-11 13:57:31 +0700

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,24 @@ socket.on('connect', () => {
 
 socket.on('newMessage', message => {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    $('#messages').append(`<li>[${formattedTime}] - ${message.from}: ${message.text}</li>`);
+    //$('#messages').append(`<li>[${formattedTime}] - ${message.from}: ${message.text}</li>`);
+    let html = Mustache.render($('#message-template').html(), {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
 })
 
 socket.on('newLocationMessage', message => {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    $('#messages').append(`<li>[${formattedTime}] - ${message.from}: <a href="${message.url}" target="_blank">My location</a></li>`)
+    //$('#messages').append(`<li>[${formattedTime}] - ${message.from}: <a href="${message.url}" target="_blank">My location</a></li>`)
+    let html = Mustache.render($('#location-message-template').html(), {
+        text: message.txt,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
 })
 socket.on('disconnect', () => {
     console.log("disconnected from server");
