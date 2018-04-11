c3d0d43ea06e425b68fa19e6b2dadf48d9e2b4e1
hungtrn75 <hungtrn75@gmail.com>
2018-04-11 14:29:43 +0700

    Scroll to bottom if user is close bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+    let messages = $('#messages');
+    let newMessage = messages.children('li:last-child');
+
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', () => {
     console.log('connected to server');
     socket.on('newEmail', (email) => {
@@ -8,7 +23,7 @@ socket.on('connect', () => {
 });
 
 socket.on('newMessage', message => {
-    let formattedTime = moment(message.createdAt).format('h:mm a');
+    let formattedTime = moment(message.createdAt).format('h:mm:ss a');
     //$('#messages').append(`<li>[${formattedTime}] - ${message.from}: ${message.text}</li>`);
     let html = Mustache.render($('#message-template').html(), {
         text: message.text,
@@ -16,6 +31,7 @@ socket.on('newMessage', message => {
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage', message => {
@@ -27,6 +43,7 @@ socket.on('newLocationMessage', message => {
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 })
 socket.on('disconnect', () => {
     console.log("disconnected from server");
